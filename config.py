dd477f8fce4be5aa78b0ea2408f7eaa14afe3d6e
Nick Wodzisz <82302381+nick-wodzisz@users.noreply.github.com>
2021-05-31 16:15:31 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+API_KEY = 'PKDDYA6L6A9BN2JXBQOY'
+API_SECRET = 'YyQjckLB3zjBdjqT2v70qK0FzjahbvZrH6qgrmnb'
+API_URL = 'https://paper-api.alpaca.markets'
+
+DB_HOST = 'localhost'
+DB_USER = 'postgres'
+DB_PASS = 'password'
+DB_NAME = 'etfdb' 
